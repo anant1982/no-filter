9c1344d0b0f08622b4da88a684a5e95c4547dd5c
Anant Ghate <sant.ghate@gmail.com>
2024-03-15 05:04:47 +0530

    Added hover effect on home page slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
 import { items } from "../utils/imageData";
 import SliderInfinite from "./SliderInfinite";
+import hoverEffect from "hover-effect";
+
 const Home = () => {
+	useEffect(() => {
+		Array.from(document.querySelectorAll(".my-div")).forEach((el) => {
+			const imgs = Array.from(el.querySelectorAll("img"));
+			new hoverEffect({
+				parent: el,
+				intensity: el.dataset.intensity || undefined,
+				speedIn: el.dataset.speedin || undefined,
+				speedOut: el.dataset.speedout || undefined,
+				easing: el.dataset.easing || undefined,
+				hover: el.dataset.hover || undefined,
+				image1: imgs[0].getAttribute("src"),
+				image2: imgs[1].getAttribute("src"),
+				displacementImage: el.dataset.displacement,
+				imagesRatio: 848 / 563,
+			});
+		});
+	}, []);
 	return (
 		<div id="wrapper" style={{ background: "#b5b5c1" }}>
 			<SliderInfinite className="slider-infinite" items={items} />
--- src/components/SliderInfinite/style.js
+++ src/components/SliderInfinite/style.js
@@ -1,92 +1,95 @@
 const style = () => {
-  const output = {
-    root: {
-      position: 'relative',
-      zIndex: 1,
-      overflow: 'hidden',
-      touchAction: 'pan-y',
-      paddingBottom: 0,
-      height: '100%',
-      '& *': {
-        cursor: 'grab',
-        userSelect: 'none',
-        userDrag: 'none',
-      },
-    },
-    slider: {
-      position: 'relative',
-      zIndex: 1,
-      height: "100%",
-      display: "flex",
-      alignItems: "center"
-    },
-    isDragging: {
-      '& *': {
-        cursor: 'grabbing',
-      },
-    },
+	const output = {
+		root: {
+			position: "relative",
+			zIndex: 1,
+			overflow: "hidden",
+			touchAction: "pan-y",
+			paddingBottom: 0,
+			height: "100%",
+			"& *": {
+				cursor: "grab",
+				userSelect: "none",
+				userDrag: "none",
+			},
+		},
+		slider: {
+			position: "relative",
+			zIndex: 1,
+			height: "100%",
+			display: "flex",
+			alignItems: "center",
+		},
+		isDragging: {
+			"& *": {
+				cursor: "grabbing",
+			},
+		},
 
-    /*------------------------------
+		/*------------------------------
     Slide
     ------------------------------*/
-    slide: {
-      position: 'absolute',
-      zIndex: 1,
-      width: '30vw',
-      height: '80vh',
-      padding: '20px',
-      '& button': {
-        width: '100%',
-        height: '100%',
-        padding: 0,
-        border: 'none',
-        background: 'none',
-        position: 'relative',
-        zIndex: 1,
-        display: 'block',
-        '--skew': 'calc(var(--speed) * .1deg)',
-        transform: 'skewX(var(--skew))',
-      },
-      '& img': {
-        width: '100%',
-        height: '100%',
-        objectFit: 'cover',
-        borderRadius: '16px',
-        '--scale': 'calc(1 + var(--speed-abs) * 0.001)',
-        '--swipe': 'calc(var(--speed) * 1px)',
-        transform: 'scaleX(var(--scale)) translateX(var(--swipe))',
-      },
+		slide: {
+			position: "absolute",
+			zIndex: 1,
+			width: "30vw",
+			height: "80vh",
+			padding: "20px",
+			"& button": {
+				width: "100%",
+				height: "100%",
+				padding: 0,
+				border: "none",
+				background: "none",
+				position: "relative",
+				zIndex: 1,
+				display: "block",
+				"--skew": "calc(var(--speed) * .1deg)",
+				transform: "skewX(var(--skew))",
+			},
+			"& img": {
+				width: "100%",
+				height: "100%",
+				objectFit: "cover",
+				borderRadius: "16px",
+				"--scale": "calc(1 + var(--speed-abs) * 0.001)",
+				"--swipe": "calc(var(--speed) * 1px)",
+				transform: "scaleX(var(--scale)) translateX(var(--swipe))",
+			},
 
-      '& h2': {
-        color: '#333',
-        margin: "10px 0 0",
-        fontSize: 25,
-        fontFamily: "\"Work Sans\", sans-serif",
-        '& span': {
-          position: 'absolute',
-          zIndex: -1,
-          left: -20,
-          bottom: -30,
-          fontSize: 100,
-          lineHeight: 1,
-          opacity: 0.2,
-        },
-      },
-    },
-  }
+			"& h2": {
+				color: "#333",
+				margin: "10px 0 0",
+				fontSize: 25,
+				fontFamily: '"Work Sans", sans-serif',
+				position: "absolute",
+				bottom: "-50px",
+				width: "100%",
+				"& span": {
+					position: "absolute",
+					zIndex: -1,
+					left: -20,
+					bottom: -30,
+					fontSize: 100,
+					lineHeight: 1,
+					opacity: 0.2,
+				},
+			},
+		},
+	};
 
-  /*------------------------------
+	/*------------------------------
   SM
   ------------------------------*/
-  output['@media (max-width: 768px)'] = {
-    slide: {
-      width: '70vw',
-      height: "80vh",
-      padding: 20,
-    },
-  }
+	output["@media (max-width: 768px)"] = {
+		slide: {
+			width: "70vw",
+			height: "80vh",
+			padding: 20,
+		},
+	};
 
-  return output
-}
+	return output;
+};
 
-export default style
+export default style;
